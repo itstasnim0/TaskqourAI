857d83ebf67bc008cbe2ce5deff91b94831afbbe
Tas <itstasnim101@gmail.com>
2026-09-15 14:48:51 +0330

    migration + user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "full_name", "is_staff", "is_active")
+    ordering = ("email",)
